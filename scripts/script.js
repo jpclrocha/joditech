e6a5acd9af4db59e59ae4b080e78ab0de4654fd5
João Pedro <77790402+jpclrocha@users.noreply.github.com>
2022-04-19 19:08:04 -0300

    show movies summary
    Added a function with javascript to show movies summary

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function mostra(id){
+    let p = document.getElementById(id);
+    p.style.display = "flex"
+}
+
+function some(id){
+    let p = document.getElementById(id);
+    p.style.display = "none"
+}
